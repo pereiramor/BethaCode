f249e341f5f7f0083fc8a6f4c9a61b86ed064e6e
pereiramor <morgana.pereirasc@gmail.com>
2021-07-03 17:48:37 -0300

    Desafio2

---
+++ AreaTrapezio.java
@@ -0,0 +1,11 @@
+public class AreaTrapezio {
+    public static void main (String [] args) {
+
+        double h = 1.60;
+        double b = 1.30;
+        double B = 2.00;
+
+        double area = (h * (b + B)) / 2;
+        System.out.println("imprime: Area trapezio:" + area);
+    }
+}
